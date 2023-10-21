2052a301639178131db99796b9274b8bf203efce
OJulianaortiz <julianaagustinaortizz@gmail.com>
2023-10-21 17:35:43 -0300

    Update Rick and Morty

--- rick_and_morty/Back-End/src/index.js
+++ rick_and_morty/Back-End/src/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const server = express();
 const PORT = 3001;
 const router = require("./routes/index");
+const { conn } = require("./DB_connection");
+
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log("Server raised in port: " + PORT);
+  });
+});
 
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -17,7 +24,3 @@ server.use((req, res, next) => {
 //middlewares
 server.use(express.json());
 server.use("/rickandmorty", router);
-
-server.listen(PORT, () => {
-  console.log("Server raised in port: " + PORT);
-});
