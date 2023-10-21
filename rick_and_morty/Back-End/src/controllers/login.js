7e1914937e4547a1d75dc0fa19d613123a9e39f5
OJulianaortiz <julianaagustinaortizz@gmail.com>
2023-10-21 19:24:44 -0300

    Update Rick and Morty

--- rick_and_morty/Back-End/src/controllers/login.js
+++ rick_and_morty/Back-End/src/controllers/login.js
@@ -1,18 +1,25 @@
-const users = require("../utils/users");
+const User = require("../utils/users");
 
-const login = (req, res) => {
+const login = async (req, res) => {
   // obetenemos datos de emial y password, desde la consulta al query
   // es hacer destructuring para quedarme con la propiedad!!
   const { email, password } = req.query;
   // verificamos si los datos del array coinciden con los de query
-  const user = users.find(
-    (user) => user.email === email && user.password === password
-  );
 
-  if (user) {
-    res.status(200).json({ access: true });
-  } else {
-    res.status(200).json({ access: false });
+  try {
+    if (email && password) {
+      const foundUser = await User.findOne({
+        where: { email },
+      });
+      if (!foundUser)
+        return res.status(404).json({ message: "Usuario no encontrado" });
+      if (foundUser.password !== password)
+        return res.status(403).json({ message: "Contraseña incorrecta" });
+      return res.status(200).json({ access: true });
+    }
+    return res.status(200).json({ message: "Flatan datos" });
+  } catch (error) {
+    return res.status(500).json({ message: error });
   }
 };
 
